55a17fc65ce275c2108bbb4bb993f8dca49d36e9
KOFF63 <Kriloff63@yandex.ru>
2025-10-05 21:26:51 +0400

    D 01

--- react/three/src/App.js
+++ react/three/src/App.js
@@ -1,36 +1,44 @@
 import React from 'react';
 import './App.css';
-import Hello from './Hello';
-import Length from './Length';
-import Form from './Form';
-import Range from './Range';
+// import Hello from './Hello';
+// import Length from './Length';
+// import Form from './Form';
+// import Range from './Range';
+// import Posts from './Posts';
+import Test from './Test';
 
-import Posts from './Posts';
-
-class App extends React.Component{
+class App extends React.Component {
   state = {
     posts: [
-      {id: '1', name: "JS Basics", title: "Обучение базовым конструкциям JavaScript"},
-      {id: '2', name: "JS Advanced", title: "Обучение расширенным возможностям JavaScript"},
-      {id: '3', name: "React JS", title: "Обучение React JS"},
+      { id: '1', name: "JS Basics", title: "Обучение базовым конструкциям JavaScript" },
+      { id: '2', name: "JS Advanced", title: "Обучение расширенным возможностям JavaScript" },
+      { id: '3', name: "React JS", title: "Обучение React JS" },
     ]
   }
 
-removePost = (id) => {
-  this.setState({posts: this.state.posts.filter(post => post.id !== id)})
-}
+  // removePost = (id) => {
+  //   this.setState({posts: this.state.posts.filter(post => post.id !== id)})
+  // }
 
-  render(){
-    let {posts} = this.state;
-      return(
-          <div className="App">
-            <Posts posts={posts} removePost={this.removePost}/>
+  componentDidMount(){
+    console.log('Component Did Mount');
+  }
+
+  componentDidUpdate(){
+    console.log('Component Did Update');
+  }
+  render() {
+    let { posts } = this.state;
+    return (
+      <div>
+        <Test/>
+        {/* <Posts posts={posts} removePost={this.removePost}/>
               <Hello />
               <Length />
               <Form/>
-              <Range/>
-          </div >
-      )
-    }    
+              <Range/> */}
+      </div >
+    )
+  }
 }
 export default App;
