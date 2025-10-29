95d109547412a4b7222aa041f3a8f134c35e6911
KOFF63 <Kriloff63@yandex.ru>
2025-10-29 15:51:27 +0400

    D-28

--- first/password_generator_project/password_generator_project/urls.py
+++ first/password_generator_project/password_generator_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('password/', views.password, name="psw"),
+    path('about/', views.about, name='about'),
 ]
